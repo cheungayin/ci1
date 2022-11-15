1054ffc1008eee1479faf9a5e9c610c4dc2bf580
Janice Cheung <cheungayin@users.noreply.github.com>
2022-11-14 22:48:31 -0500

    initial commit
    entry9 trial still need to work on this unclear how to click to open "tabs"

---
+++ entries/entry9trial/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+
+	$(".item_title").click(function () {
+		var this_id = $(this).attr("data-id");
+		if ($(this).hasClass("on")) {
+			$(this).removeClass('on');
+			$(".topitem_box").removeClass("show");
+			$("#" + this_id).removeClass("on");
+
+			return false;
+
+		};
+		$(".item_title").removeClass("on");
+		$(this).addClass("on");
+		$(".topitem").removeClass("on");
+		$("#" + this_id).addClass('on');
+		$(".topitem_box").addClass('show');
+
+	});
+
+	$(".area").click(function () {
+		if ($(this).hasClass("item_open")) {
+			$(this).removeClass("item_open");
+			$(this).addClass("item");
+			return false;
+		};
+		$(".area").removeClass("item_open");
+		$(".area").addClass("item");
+		$(this).removeClass("item");
+		$(this).addClass("item_open");
+	});
+});
